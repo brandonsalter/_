a8dced348fff8fcf38a2b73cfae06b21071b6875
Brandon Salter <brandonsalter111@gmail.com>
2023-08-19 08:51:10 -0400

    intitial pred method

--- lin_reg.py
+++ lin_reg.py
@@ -14,12 +14,12 @@ class LR:
         if df.iloc[:,0].sum() != len(df): # if there's no intercept column
             df.insert(loc = 0, column = 'x0', value = np.ones(len(df)))
         A = df.drop(label, axis=1).to_numpy()
-        # Solve A_T_A x = A_T b for x
+        # Solve A_T_A x_hat = A_T b for x_hat
         A_T = np.transpose(A)
         A_T_A = np.matmul(A_T, A)
         A_T_b = np.matmul(A_T, b)
         x = np.linalg.solve(A_T_A, A_T_b)
-        # Solve for p and compute sse
+        # Solve Ax_hat = p (p in ColA) and compute sse
         p = np.matmul(A, x)
         e = b-p
         train_sse = (np.linalg.norm(e))**2.
@@ -27,3 +27,11 @@ class LR:
         intercept = x[0]
         coefficients = x[1:]
         return intercept, coefficients, train_sse
+    
+    def predict(self, test_df: pd.DataFrame, label: str, coefficients: np.array, intercept: np.array):
+        test_matrix = test_df.drop(label, axis=1).to_numpy()
+        y_actual = test_df[label].to_numpy()
+        y_pred = np.dot(test_matrix, coefficients)
+        e = y_actual - y_pred
+        test_sse = (np.linalg.norm(e))**2.
+        return y_pred, test_sse
